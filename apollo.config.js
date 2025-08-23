1b86b7234ad99d100ca70dcbb18def4b191c2488
Robert-d-s <110900093+Robert-d-s@users.noreply.github.com>
2025-08-23 09:37:24 +0200

    .

--- apollo.config.js
+++ apollo.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "enablment_front",
       // prefer environment override in Next apps
       url:
-        process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:3000/graphql",
+        process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:8080/graphql",
     },
     includes: ["src/**/*.{ts,tsx,js,jsx,graphql,gql}"],
   },
